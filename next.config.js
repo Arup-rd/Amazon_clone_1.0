75d4bbced3ae89c71853a6dc2583bbbd60eaa9b0
Arup rd <aruprd38@gmail.com>
2023-03-03 02:22:03 -0800

    Added Header Component

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     experimental: {
       appDir: true,
     },
   }
+
+  const images = {
+    domains: ["links.papareact.com", "fakestoreapi.com"]
+  }
   
-  module.exports = nextConfig
+  module.exports = {
+    nextConfig,
+    images
+  }
